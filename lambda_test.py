9a5620f2172f05817e7b2fe244d2bacd029965ea
Aleksey Kislitsa <aleksey.kislitsa@gmail.com>
2023-01-21 15:09:44 +0300

    Set function generator

--- lambda_test.py
+++ lambda_test.py
@@ -1,7 +1,11 @@
-# put your python code here
-from math import *
-a = 3#int(input())
-b = 4#int(input())
-c = 5#int(input())
-p = (a + b + c)/2
-print(sqrt(p*(p - a)*(p - b)*(p - c)))
+xs = [1,0,1]
+# fs = lambda xs: [lambda x : xs[n]*pow(x,n) for n in range(len(xs))]
+# fs = lambda a, n: lambda x : a*pow(x,n)
+# ts = [fs(xs[n],n) for n in range(len(xs))]
+fs = [(lambda a, n: lambda x : a*pow(x,n))(xs[n],n) for n in range(len(xs))]
+
+# ffs = lambda x : sum([f(x) for f in [(lambda a, n: lambda x : a*pow(x,n))(xs[n],n) for n in range(len(xs))]])
+# ffs = lambda x : [f(x) for f in [fs(xs[n],n) for n in range(len(xs))]]
+ffs = lambda x : sum([f(x) for f in fs])
+
+print(ffs(2))
