fb5989d46365839b7350515d9e3439108ca4c98b
Egor Liovkin <egor.liovkin@gmail.com>
2023-02-11 18:57:46 +0300

    Added  test and improved array_diff

--- codewars/diff_arr.py
+++ codewars/diff_arr.py
@@ -1,3 +1,6 @@
 def array_diff(a, b):
     #your code here
-    return [x for x in a if (lambda x,xs: list(filter(lambda y: x == y, xs)) == [])(x,b)]
+    return [x for x in a if not x in b]
+
+print(array_diff([1,2],[1]) == [2])
+print(array_diff([1,2,2,2,3],[2]) == [1,3])
