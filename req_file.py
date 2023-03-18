d029c0451b6695bd650c2b678af86fd1e456de9c
Egor Liovkin <egor.liovkin@gmail.com>
2023-03-18 17:23:51 +0300

    Added answer

--- req_file.py
+++ req_file.py
@@ -24,3 +24,10 @@ while line[:2] != "We":
     print(line)
     # time.sleep(1)
     
+# We are the champions, my friends,
+# And we'll keep on fighting 'til the end.
+# We are the champions.
+# We are the champions.
+# No time for losers
+# 'Cause we are the champions of the world.
+    
