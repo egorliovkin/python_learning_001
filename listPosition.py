dd1c6426bffbe6a5e7a06955a8a4357ff6c9171b
Egor Liovkin <egor.liovkin@gmail.com>
2023-05-13 20:01:04 +0300

    Codewars listPosition soldev

--- listPosition.py
+++ listPosition.py
@@ -4,12 +4,25 @@ def listGen(xs):
     elif len(xs) == 1:
         return set(xs)
     elif len(xs) == 2:
-        return set([xs,xs[::-1]])
+        return set(sorted([xs,xs[::-1]]))
     else:
         res = set([])
         for i in range(len(xs)):
             bs = xs[:i] + xs[i+1:]
-            [res.add(x) for x in [xs[i] + x for x in listGen(bs)]]
+            # [res.add(x) for x in [xs[i] + x for x in listGen(bs)]]
+            res.update([xs[i] + x for x in sorted(listGen(bs))])
         return res
 
+def listPosition(x):
+    return sorted(listGen(x)).index(x) + 1
+
+# print(listPosition('ABAB'))
+# print(listPosition('AAAB'))
+# print(listPosition('BAAA'))
+# print(listPosition('QUESTION'))
+# print(listPosition('BOOKKEEPER'))
+
+print(listGen('ABAB'))
 print(sorted(listGen('ABAB')))
+# print(listGen(''.join(sorted('QUESTION'))))
+# print(listGen(''.join(sorted('BOOKKEEPER'))))
