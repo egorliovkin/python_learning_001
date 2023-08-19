be03aff1ec8180dd8722560e55e77510a9702c78
Egor Liovkin <egor.liovkin@gmail.com>
2023-08-19 16:31:24 +0300

    Next optimisation

--- bin_pow.py
+++ bin_pow.py
@@ -6,8 +6,11 @@
 # else:
 #     print("НЕТ")
 
-xs = bin(int(input()))[3:]
-if sum([int(x) for x in xs]) == 0:
-    print(len(xs))
-else:
-    print("НЕТ")
+# xs = bin(int(input()))[3:]
+# if sum([int(x) for x in xs]) == 0:
+#     print(len(xs))
+# else:
+#     print("НЕТ")
+
+xs = [int(x) for x in bin(int(input()))[3:]]
+print(len(xs)) if sum(xs) == 0 else print("НЕТ")
