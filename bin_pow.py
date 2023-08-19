60b2439a759d88df53db7c74b56dceda7de13914
Egor Liovkin <egor.liovkin@gmail.com>
2023-08-19 16:24:18 +0300

    Optimized pow checking

--- bin_pow.py
+++ bin_pow.py
@@ -1,7 +1,13 @@
-import math
+# import math
 
-b = math.log(a := int(input()),2)
-if math.pow(2, int(b)) == a:
-    print(int(b))
+# b = math.log(a := int(input()),2)
+# if math.pow(2, int(b)) == a:
+#     print(int(b))
+# else:
+#     print("НЕТ")
+
+xs = bin(int(input()))[3:]
+if sum([int(x) for x in xs]) == 0:
+    print(len(xs))
 else:
     print("НЕТ")
